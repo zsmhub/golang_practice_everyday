c374e55b8bbea2bb18d6c2a2a77df83f9aa85af1
Insomnia Zhang <zhangshimiande@gmail.com>
2020-04-20 11:39:56 +0800

    goroutine 竞争条件代码优化

--- main/goroutine_contend_condition.go
+++ main/goroutine_contend_condition.go
@@ -1,6 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// 多服务器模式-使用redis控制共享变量
+
+// 单服务器模式-方案一：使用互斥锁或读写锁来处理goroutine的竞争条件
+func main() {
+	startTime := time.Now().UnixNano()
+
+	nums := 0
+	total := 0
+
+	wg := sync.WaitGroup{}
+	wg.Add(100000)
+
+	var mu sync.Mutex
+
+	for i := 1; i <= 100000; i++ {
+		nums += i
+
+		go func(i int) {
+			mu.Lock()
+			total += i
+			mu.Unlock()
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("total:%d sum %d\n", total, nums)
+
+	endTime := time.Now().UnixNano()
+	fmt.Println("耗时(纳秒)：", endTime-startTime)
+}
+
+// 单服务器模式-方案二：相比方案一，效率低
+/*package main
+
+import (
+	"fmt"
+	"sync"
+)
 
 // 如何在程序中避免竞争条件呢？
 // 方案：【采用channel方式】避免从多个 goroutine 访问同一个变量，例如创建一个唯一能够访问该变量的 goroutine，
@@ -10,12 +55,6 @@ import "fmt"
 var num int = 100
 var deposits = make(chan int) // 存款
 var balances = make(chan int) // 金额
-var sigovers = make(chan struct{}, num)
-
-func Deposit(amount int) {
-	deposits <- amount
-	sigovers <- struct{}{}
-}
 
 func teller() {
 	var balance int
@@ -30,9 +69,18 @@ func teller() {
 
 func main() {
 	go teller()
+
+	var wg sync.WaitGroup
+	wg.Add(num)
+
 	for i := 0; i < num; i++ {
-		go Deposit(100)
-		<-sigovers
+		go func(amount int) {
+			deposits <- amount
+			wg.Done()
+		}(100)
 	}
+
+	wg.Wait()
+
 	fmt.Printf("balance = %d\n", <-balances)
-}
+}*/
