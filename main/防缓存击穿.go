dbba3e31649cf77f358459f9af18776279d550f2
zsm <zhangshimiande@gmail.com>
2020-09-05 16:10:09 +0800

    refactor: 防缓存击穿代码优化

--- main/防缓存击穿.go
+++ main/防缓存击穿.go
@@ -12,7 +12,7 @@ import (
 var count = int64(0)
 
 func a() (interface{}, error) {
-    //time.Sleep(time.Millisecond * 500)
+    //time.Sleep(time.Millisecond * 500) // 去掉注释可查看高并发取值情况
     return atomic.AddInt64(&count, 1), nil
 }
 
@@ -25,12 +25,19 @@ func main() {
         wg.Add(1)
         go func(j int) {
             defer wg.Done()
+
+            // 直接返回结果集
             val, err, shared := g.Do("cache_key_name", a)
             if err != nil {
                 fmt.Println(err)
                 return
             }
-            fmt.Printf("index: %d, val: %d, shard: %v\n", j, val, shared)
+            fmt.Printf("index: %d, val: %d, shared: %v\n", j, val, shared)
+
+            // 返回值为 channel 方式
+            //result := g.DoChan("cache_key_name2", a)
+            //ret, _ := <-result
+            //fmt.Printf("channel->index: %d, val: %d, shared: %v\n", j, ret.Val, ret.Shared)
         }(i)
     }
 
